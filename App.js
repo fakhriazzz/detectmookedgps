c222445d8e8f5bf513488c6f57c6a51bf897f49d
Fakhri Azmi Husaini <rifakhrii@gmail.com>
2024-11-08 10:25:04 +0700

    import Geolocation from 'react-native-geolocation-service'

--- App.js
+++ App.js
@@ -1,19 +1,76 @@
-import JailMonkey from 'jail-monkey'
 import React, { useEffect, useState } from 'react'
-import { Text, View } from 'react-native'
+import { PermissionsAndroid, Platform, Text, View } from 'react-native'
+import Geolocation from 'react-native-geolocation-service'
 
 const App = () => {
   const [status, setstatus] = useState('')
+  const requestLocationPermissionAndroid = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "Location Permission",
+          message: "This app needs access to your location.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        Geolocation.getCurrentPosition(
+          (position) => {
+            console.log(position.mocked);
+            setstatus(position.mocked)
+          },
+          (error) => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+          },
+          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+        // Call Geolocation.getCurrentPosition here
+      } else {
+        console.log("Location permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  const requestLocationPermissionIOS = () => {
+    Geolocation.requestAuthorization('whenInUse').then((status) => {
+      if (status === 'granted') {
+        // Izin diberikan, panggil Geolocation.getCurrentPosition
+        Geolocation.getCurrentPosition(
+          (position) => {
+            console.log(position);
+            setstatus(position.mocked)
+          },
+          (error) => {
+            console.log(error.code, error.message);
+          },
+          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+      } else {
+        console.log('Location permission not granted');
+      }
+    });
+  }
 
   useEffect(() => {
-    if (JailMonkey.isJailBroken()) {
-      setstatus('root')
-    } else if (JailMonkey.canMockLocation) {
-      setstatus('canMockLocation')
-    } else if (JailMonkey.isDebuggedMode) {
-      setstatus('isDebuggedMode')
+    // if (JailMonkey.isJailBroken()) {
+    //   setstatus('root')
+    // } else if (JailMonkey.canMockLocation) {
+    //   setstatus('canMockLocation')
+    // } else if (JailMonkey.isDebuggedMode) {
+    //   setstatus('isDebuggedMode')
+    // } else {
+    //   setstatus('aplikasi aman')
+    // }
+    if (Platform.OS == 'android') {
+      requestLocationPermissionAndroid()
     } else {
-      setstatus('aplikasi aman')
+      requestLocationPermissionIOS()
     }
   }, [])
 
